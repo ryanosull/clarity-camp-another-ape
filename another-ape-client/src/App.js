ec84ec32cb17f3f6711f0910da96e5ecc899a0e1
William Ryan O'Sullivan <wrosullivan88@gmail.com>
2024-05-23 14:45:46 -0500

    docker, fleek, stacks js

--- another-ape-client/src/App.js
+++ another-ape-client/src/App.js
@@ -1,50 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 import ConnectWallet from "./components/ConnectWallet";
-import ContractCallVote from "./components/ContractCallVote";
+import Mint from "./components/Mint";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <h2>React + Stacks.js 👋</h2>
-
-        {/* ConnectWallet file: `./src/components/ConnectWallet.js` */}
+        <h2> Another Ape NFT </h2>
         <ConnectWallet />
-
-        {/* ContractCallVote file: `./src/components/ContractCallVote.js` */}
-        <ContractCallVote />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://docs.hiro.so/intro"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn Stacks
-        </a>
-        <a
-          className="App-link"
-          href="https://docs.hiro.so/build-apps/overview"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn to Build on Stacks
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Mint />
       </header>
     </div>
   );
